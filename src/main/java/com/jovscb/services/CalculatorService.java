f3517422fff6d7cb366333b461c9acd4c6314f7a
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-11-04 17:40:45 +0800

    add sample test class

---
+++ src/main/java/com/jovscb/services/CalculatorService.java
@@ -0,0 +1,7 @@
+package com.jovscb.services;
+
+public class CalculatorService {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
--- src/test/java/com/jovscb/JenkinsMavenProjectApplicationTests.java
+++ src/test/java/com/jovscb/JenkinsMavenProjectApplicationTests.java
@@ -1,13 +1,22 @@
 package com.jovscb;
 
+import com.jovscb.services.CalculatorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JenkinsMavenProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testAdd() {
+		CalculatorService service = new CalculatorService();
+		assertEquals(10, service.add(5,5));
 	}
 
 }
