fe1ac3f24a271b52d06d3d4f1288b7f5bddec9ef
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-11-04 17:25:26 +0800

    create project

---
+++ src/main/java/com/jovscb/JenkinsMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jovscb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMavenProjectApplication.class, args);
+	}
+
+}
